98cea7cfae9cd509375ddb66cad14d57ba323cb8
QuinceyYang <yqing555@qq.com>
2018-09-13 16:40:00 +0800

    Initial Commit

---
+++ YQCustomKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQCustomKeyboard
+//
+//  Created by 杨清 on 2018/9/13.
+//  Copyright © 2018年 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQCustomKeyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YQCustomKeyboard
+//
+//  Created by 杨清 on 2018/9/13.
+//  Copyright © 2018年 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
